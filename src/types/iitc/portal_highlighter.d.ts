676eac663f79557c84b8943fd7cbab63e822dc9c
McBen <viertelvor12@gmx.net>
2022-07-10 12:19:25 +0200

    highlighter can be undefined

--- src/types/iitc/portal_highlighter.d.ts
+++ src/types/iitc/portal_highlighter.d.ts
@@ -6,21 +6,21 @@ declare global {
     }
 
     /** an object mapping highlighter names to the object containing callback functions */
-    var _highlighters: { [name: string]: Hightligher } | null;
+    let _highlighters: { [name: string]: Hightligher } | null;
 
     /** the name of the current highlighter */
-    var _current_highlighter: string;
+    let _current_highlighter: string | undefined;
 
     /**
      * = "No Highlights"
      */
-    var _no_highlighter: string;
+    let _no_highlighter: string;
 
     function addPortalHighlighter(name: string, data: Hightligher): void;
 
     // (re)creates the highlighter dropdown list
     function updatePortalHighlighterControl(): void;
-    function changePortalHighlights(name: string): void;
+    function changePortalHighlights(name: string | undefined): void;
 
     function highlightPortal(p: IITC.Portal): void;
     function resetHighlightedPortals(): void;
