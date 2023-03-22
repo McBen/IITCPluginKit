364f87b88b8c87415b6398bbbeb091490d36d4d2
McBen <viertelvor12@gmx.net>
2023-03-22 10:08:01 +0100

    style: fix interface (, instead of ;)

--- types/iitc/portal_info.d.ts
+++ types/iitc/portal_info.d.ts
@@ -30,13 +30,13 @@ declare global {
     function getLinkAmpRangeBoost(d: IITC.PortalDataDetail): number;
 
     function getAttackApGain(d: IITC.PortalDataDetail, fieldCount: number, linkCount: number): {
-        friendlyAp: number;
-        deployCount: number;
-        upgradeCount: number;
-        enemyAp: number;
-        destroyAp: number;
-        resoAp: number;
-        captureAp: number;
+        friendlyAp: number,
+        deployCount: number,
+        upgradeCount: number,
+        enemyAp: number,
+        destroyAp: number,
+        resoAp: number,
+        captureAp: number
     };
 
     /** This function will return the potential level a player can upgrade it to */
@@ -53,7 +53,8 @@ declare global {
         shields: number,
         links: number,
         linkDefenseBoost: number,
-        excess: number;
+        total: number,
+        excess: number
     };
     function getMaxOutgoingLinks(d: IITC.PortalDataDetail): number;
 
@@ -67,20 +68,20 @@ declare global {
      * given a detailed portal structure, return summary portal data, as seen in the map tile data
      */
     function getPortalSummaryData(d: IITC.PortalDataDetail): {
-        level: number;
-        title: string;
-        image: string;
-        resCount: number;
-        latE6: number;
-        health: number;
-        team: number;
-        lngE6: number;
+        level: number,
+        title: string,
+        image: string,
+        resCount: number,
+        latE6: number,
+        health: number,
+        team: number,
+        lngE6: number,
         type: 'portal'
     };
 
     function getPortalAttackValues(d: IITC.PortalDataDetail): {
-        hit_bonus: number;
-        force_amplifier: number;
-        attack_frequency: number;
+        hit_bonus: number,
+        force_amplifier: number,
+        attack_frequency: number
     };
 }
