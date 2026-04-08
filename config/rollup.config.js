72ef88110176580bec31158077af16b0fd87fcfe
McBen <viertelvor12@gmx.net>
2026-04-08 17:12:24 +0200

    ipk: add unix shell run command

---
+++ config/rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from "@rollup/plugin-typescript";
+import nodeExternals from "rollup-plugin-node-externals";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/ipk.ts",
+  output: {
+    dir: "dist",
+    format: "es",
+    banner: "#!/usr/bin/env node",
+  },
+  plugins: [
+    typescript(),
+    nodeExternals(),
+    // terser({ compress: false, mangle: false, format: { beautify: true } }),
+  ],
+  external: ["node"],
+};
